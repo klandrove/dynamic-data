b492c1b07756152024f97dd2b7524e72994ae609
KaitLand12 <kmlandrove@gmail.com>
2024-10-24 22:32:50 -0400

    Add Midterm

--- miami/site/miami.js
+++ miami/site/miami.js
@@ -1,7 +1,19 @@
+// Sections descriptions:
+
+// About: 1 short paragraph per page
+
+// Services: show at least 4 services per page
+
+// Packages: show at least 6 packages per page
+
+// Gallery: show at least 16 images per page
+
 // imports express into our project
 const express = require('express')
 // create the express server inside a variable called app
 const app = express()
+// Specify static routes
+app.use(express.static('public'))
 // import a package for handlebars
 const expressHandlebars = require('express-handlebars')
 // make express use the handlebars template engine
@@ -11,27 +23,44 @@ app.engine('handlebars', expressHandlebars.engine({
 app.set('view engine','handlebars')
 
 const PORT = process.env.port || 3000
+//Import app-wide data
+const gallery = require("./data/gallery.json")
 // Process routes
+const data = require("./data/home-data.json")
 app.get('/', (request,response)=> {
-    response.render('home')
+    response.render('landing', {
+        gallery,
+        data
+    })
 })
-app.get('/about', (request,response)=> {
-    response.render('about')
+const history_data = require("./data/history-data.json")
+app.get('/history', (request,response)=> {
+    response.render('page',{
+        gallery,
+        data:history_data
+    })
 })
+const art_data = require("./data/art-data.json")
+app.get('/art', (request,response)=> {
+    response.render('page',{
+        gallery,
+        data:art_data
+    })
+})
+const nightlife_data = require("./data/nightlife-data.json")
 app.get('/nightlife', (request,response)=> {
-    response.type('text/plain')
-    response.send('Miami at Night: Miami is known for its nightlife, especially in South Beach.')
+    response.render('page',{
+        gallery,
+        data:nightlife_data
+    })
 })
-app.get('/beaches', (request,response)=> {
-    response.type('text/plain')
-    response.send('Miami Beaches: Miami is home to some of the most beautiful beaches in the world.')
+const adventure_data = require("./data/adventure-data.json")
+app.get('/adventure', (request,response)=> {
+    response.render('page',{
+        gallery,
+        data:adventure_data
+    })
 })
-// this triggers a server error
-app.get('/history', (req,res)=> {
-    response.type('text/plain')
-    response.send('History of Miami')
-})
-// Handle the error first
 
 // NOT FOUND!
 app.use((request,response)=>{
